7ad42819528c6bea06822bb74814621f212c64c9
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-06-03 15:23:41 +0530

    solved in 10 seconds

---
+++ 800/String/codeforcesCheck.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+    while(t--){
+    char c;
+    cin >> c;
+
+    string s = "codeforces";
+
+    cout <<((s.find(c) != string::npos)?"YES\n":"NO\n");
+        
+    
+
+    
+
+  }
+
+    return 0;
+ }
