5cfd6eca71359fd33d0f9300f1fca5cb6e05c744
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-06-09 18:02:34 +0530

    forgot to push

---
+++ 800/Math/grassHopperLine.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int x, k;
+        cin >> x >> k;
+
+        if(x % k) {
+            cout << 1 << '\n';
+            cout << x << '\n';
+        } else {
+            cout << 2 << '\n';
+            cout << 1 << ' ' << x - 1 << '\n';
+        }
+    }
+}
