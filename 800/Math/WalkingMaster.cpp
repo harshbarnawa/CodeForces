9fe1ca4a3c7e301e9dcb5f4544d057f234ba27a7
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-06-11 21:21:50 +0530

    forgot pushing rating this 6/10

---
+++ 800/Math/WalkingMaster.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin >> t;
+
+    while(t--) {
+        long long a, b, c, d;
+        cin>> a >> b >> c >> d;
+
+        long long diag = d - b;
+
+        if(diag < 0){
+            cout << -1 << '\n';
+            continue;
+        }
+
+        long long left = a + diag - c;
+        if(left < 0){
+            cout << -1 << '\n';
+            continue;
+        }
+
+        cout << diag + left << '\n';
+    }
+}
