b8a680a7aa07e99f58e7bb1c95b478370abc806b
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-06-16 19:52:37 +0530

    simple ques rating 2/10

---
+++ 800/Math/goalsVictory.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        long long sum = 0;
+
+        for(int i = 0; i < n - 1; i++){
+            long long x;
+            cin >> x;
+            sum += x;
+        }
+        cout << -sum << '\n';
+    }
+
+    return 0;
+}
