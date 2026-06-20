7cbcbb837748bded9855c43cd1db2a99fbc7f703
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-06-20 10:11:57 +0530

    solved for routinne

---
+++ 800/Implementation/Tram.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int curr = 0, ans = 0;
+
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        curr -= a;
+        curr += b;
+
+        ans = max(ans, curr);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
