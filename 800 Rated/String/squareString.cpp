90dc0d16fb9ed8b7c91dd3172da4f1526becc18c
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-09-07 12:10:23 +0530

    string ques after so long

---
+++ 800 Rated/String/squareString.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        int n = s.size();
+
+        if (n % 2 != 0) {
+            cout << "NO\n";
+        } else {
+            string a = s.substr(0, n / 2);
+            string b = s.substr(n / 2);
+
+            if (a == b)
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
